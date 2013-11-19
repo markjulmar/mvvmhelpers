d195c61a17885be65dff6a7f6b99a27599d5bdff
markjulmar_cp <SND\markjulmar_cp>
2013-11-19 08:14:09 -0800

    Bumped version number for NuGet

--- Julmar.Wpf.Helpers/Julmar.Wpf.Helpers/Properties/AssemblyInfo.cs
+++ Julmar.Wpf.Helpers/Julmar.Wpf.Helpers/Properties/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompanyAttribute("JulMar Entertainment Technology, Inc.")]
 [assembly: AssemblyProductAttribute("JulMar.Wpf.Helpers")]
 [assembly: AssemblyCopyrightAttribute("(C) 2008-2011 JulMar Entertainment Technology, Inc.")]
-[assembly: AssemblyVersionAttribute("4.2.2.*")]
-[assembly: AssemblyFileVersionAttribute("4.2.2.0")]
+[assembly: AssemblyVersionAttribute("4.2.3.*")]
+[assembly: AssemblyFileVersionAttribute("4.2.3.0")]
 [assembly: ComVisibleAttribute(false)]
